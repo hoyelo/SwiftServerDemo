1123d7d1f30c4cfa3dd06de625fca2bc01aa3ef0
Victor Zhu <ghoyelo@gmail.com>
2016-05-10 16:50:45 +0800

    Base project

---
+++ SwiftServerDemo/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  SwiftServerDemo
+//
+//  Created by Victor Zhu on 5/10/16.
+//  Copyright © 2016 Interactivelabs. All rights reserved.
+//
+
+import Foundation
+import Vapor
+
+print("SwiftServerIO -- starting")
+
+Route.get("hello") { _ in
+	return ["Hello" : "World"]
+}
+
+Route.get("hello/:name") { request in
+	let name = request.parameters["name"] ?? "World"
+	return ["Hello" : name]
+}
+
+let server = Server()
+server.run(port: 8080)
